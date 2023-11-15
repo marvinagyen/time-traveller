7d0194f5e8dea68201342d1aa118661eabbb03ec
lnguyen2693 <n9linh@gmail.com>
2023-11-14 19:49:31 -0500

    GameHandler and PauseMenu

--- CS23_Final/Assets/Scripts/GameHandler_PauseMenu.cs
+++ CS23_Final/Assets/Scripts/GameHandler_PauseMenu.cs
@@ -33,12 +33,14 @@ public class GameHandler_PauseMenu : MonoBehaviour {
                                 Resume();
                         }
                         else{
+                                Debug.Log("Pausing!");
                                 Pause();
                         }
                 }
         }
 
         void Pause(){
+                Debug.Log(pauseMenuUI);
                 pauseMenuUI.SetActive(true);
                 Time.timeScale = 0f;
                 GameisPaused = true;
